e728ce6c8d811a63aaef575b6f093642396de0fe
Brendan Forster <me@brendanforster.com>
2013-08-03 14:59:08 -0700

    update all the things

--- CutRelease.ps1
+++ CutRelease.ps1
@@ -4,4 +4,6 @@ $outputPackage = (gci .\bin\Release -filter TestApp*.nupkg)[0].FullName
 
 write-host "Creating a release package using $outputPackage"
 
+# TODO: invoke Create-Release with required parameters?
+
 .\packages\Shimmer.0.6.1.0-beta\tools\CreateReleasePackage.exe --output-directory=".\artifacts" $outputPackage
