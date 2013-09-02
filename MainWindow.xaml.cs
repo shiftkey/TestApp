f71b6e723b68c94aa5c69cda3ef3c4c134bcd883
Brendan Forster <brendan@github.com>
2013-09-02 20:09:17 +1000

    dropped dependencies on ReactiveUI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Reflection;
-using System.Windows;
-using ReactiveUI;
-using ReactiveUI.Xaml;
-using Shimmer.Client;
+﻿using System.Windows;
+using System.Windows.Input;
 
 namespace TestApp
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -19,86 +12,4 @@ namespace TestApp
             InitializeComponent();
         }
     }
-
-    public class MainWindowViewModel : ReactiveObject
-    {
-        string _Version;
-        public string Version
-        {
-            get { return _Version; }
-            set { this.RaiseAndSetIfChanged(x => x.Version, value); }
-        }
-
-        string _UpdatePath;
-        public string UpdatePath
-        {
-            get { return _UpdatePath; }
-            set { this.RaiseAndSetIfChanged(x => x.UpdatePath, value); }
-        }
-
-        UpdateInfo _UpdateInfo;
-        public UpdateInfo UpdateInfo
-        {
-            get { return _UpdateInfo; }
-            set { this.RaiseAndSetIfChanged(x => x.UpdateInfo, value); }
-        }
-
-        UpdateInfo _DownloadedUpdateInfo;
-        public UpdateInfo DownloadedUpdateInfo
-        {
-            get { return _DownloadedUpdateInfo; }
-            set { this.RaiseAndSetIfChanged(x => x.DownloadedUpdateInfo, value); }
-        }
-
-        public ReactiveAsyncCommand CheckForUpdate { get; protected set; }
-        public ReactiveAsyncCommand DownloadReleases { get; protected set; }
-        public ReactiveAsyncCommand ApplyReleases { get; protected set; }
-
-        public MainWindowViewModel()
-        {
-            var noneInFlight = new BehaviorSubject<bool>(false);
-            var updateManager = default(UpdateManager);
-
-            this.WhenAny(x => x.UpdatePath, x => x.Value)
-                .Where(x => !String.IsNullOrWhiteSpace(x))
-                .Throttle(TimeSpan.FromMilliseconds(700), RxApp.DeferredScheduler)
-                .Subscribe(x =>
-                {
-                    if (updateManager != null) updateManager.Dispose();
-                    updateManager = new UpdateManager(UpdatePath, "TestApp", FrameworkVersion.Net40);
-                });
-
-            CheckForUpdate = new ReactiveAsyncCommand(noneInFlight);
-            CheckForUpdate.RegisterAsyncObservable(_ => updateManager.CheckForUpdate())
-                .Subscribe(x =>
-                {
-                    UpdateInfo = x;
-                    DownloadedUpdateInfo = null;
-                });
-
-            DownloadReleases = new ReactiveAsyncCommand(noneInFlight.Where(_ => UpdateInfo != null));
-            DownloadReleases.RegisterAsyncObservable(_ => updateManager.DownloadReleases(UpdateInfo.ReleasesToApply))
-                .Subscribe(_ =>
-                {
-                    DownloadedUpdateInfo = UpdateInfo;
-                });
-
-            ApplyReleases = new ReactiveAsyncCommand(noneInFlight.Where(_ => DownloadedUpdateInfo != null));
-            ApplyReleases.RegisterAsyncObservable(_ => updateManager.ApplyReleases(DownloadedUpdateInfo));
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            Version = fvi.FileVersion;
-
-            Observable.CombineLatest(
-                CheckForUpdate.ItemsInflight.StartWith(0),
-                DownloadReleases.ItemsInflight.StartWith(0),
-                ApplyReleases.ItemsInflight.StartWith(0),
-                this.WhenAny(x => x.UpdatePath, _ => 0),
-                (a, b, c, _) => a + b + c
-            ).Select(x => x == 0 && UpdatePath != null).Multicast(noneInFlight).Connect();
-        }
-    }
-
-
 }
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using GalaSoft.MvvmLight.Command;
+using Shimmer.Client;
+using TestApp.Annotations;
+
+namespace TestApp
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            CheckForUpdateCommand = new RelayCommand(CheckForUpdate, CanCheckForUpdate);
+            DownloadReleasesCommand = new RelayCommand(DownloadReleases, CanDownloadReleases);
+            ApplyReleasesCommand = new RelayCommand(ApplyReleases, CanApplyReleases);
+        }
+
+        string version;
+        public string Version
+        {
+            get { return version; }
+            set
+            {
+                if (version == value) return;
+
+                version = value;
+                OnPropertyChanged();
+            }
+        }
+
+        string updatePath;
+        public string UpdatePath
+        {
+            get { return updatePath; }
+            set
+            {
+                if (updatePath == value) return;
+
+                OnPropertyChanged();
+                updatePath = value;
+
+                UpdateInfo = null;
+                DownloadedUpdateInfo = null;
+
+                CheckForUpdateCommand.RaiseCanExecuteChanged();
+                DownloadReleasesCommand.RaiseCanExecuteChanged();
+                ApplyReleasesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        UpdateInfo updateInfo;
+        public UpdateInfo UpdateInfo
+        {
+            get { return updateInfo; }
+            set
+            {
+                if (updateInfo == value) return;
+
+                updateInfo = value;
+                OnPropertyChanged();
+
+                DownloadedUpdateInfo = null;
+
+                DownloadReleasesCommand.RaiseCanExecuteChanged();
+                ApplyReleasesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        UpdateInfo downloadedUpdateInfo;
+        public UpdateInfo DownloadedUpdateInfo
+        {
+            get { return downloadedUpdateInfo; }
+            set
+            {
+                if (downloadedUpdateInfo == value) return;
+
+                downloadedUpdateInfo = value;
+                OnPropertyChanged();
+
+                ApplyReleasesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public RelayCommand CheckForUpdateCommand { get; set; }
+
+        async void CheckForUpdate()
+        {
+            using (var updater = new UpdateManager(UpdatePath, "TestApp", FrameworkVersion.Net45))
+            {
+                UpdateInfo = await updater.CheckForUpdate();
+            }
+        }
+
+        bool CanCheckForUpdate()
+        {
+            return !String.IsNullOrWhiteSpace(UpdatePath);
+        }
+
+        public RelayCommand DownloadReleasesCommand { get; set; }
+
+        async void DownloadReleases()
+        {
+            using (var updater = new UpdateManager(UpdatePath, "TestApp", FrameworkVersion.Net45))
+            {
+               await updater.DownloadReleases(UpdateInfo.ReleasesToApply);
+               DownloadedUpdateInfo = UpdateInfo;
+            }
+        }
+
+        bool CanDownloadReleases()
+        {
+            return UpdateInfo != null;
+        }
+
+        public RelayCommand ApplyReleasesCommand { get; set; }
+
+        async void ApplyReleases()
+        {
+            using (var updater = new UpdateManager(UpdatePath, "TestApp", FrameworkVersion.Net45))
+            {
+                await updater.ApplyReleases(DownloadedUpdateInfo);
+            }
+        }
+
+        bool CanApplyReleases()
+        {
+            return DownloadedUpdateInfo != null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
